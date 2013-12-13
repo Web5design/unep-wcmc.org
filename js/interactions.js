f829508ab4593806ac58ebec63f96149c807273f
Blanca Tortajada <tortajadablanca@gmail.com>
2013-12-13 12:16:49 +0000

    staff search input and styling tweaks

--- js/interactions.js
+++ js/interactions.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
   $('button#trigger-menu').click(function() {
-    $('ul#button-menu').toggle('slow');
+    $('ul#button-menu').animate({height:"toggle"}, 500, function() {});
   });
+  
   $('button#content-button').click(function() {
     $('ul#content-small').toggle('slow');
     $('button#content-button').css({"background":"#4c9cda","color":"#fff", "border":"1px solid #4c9cda"});
   });
+  
   $('button#fields-button').click(function() {
     $('ul#fields-small').toggle('slow');
     $('button#fields-button').css({"background":"#4c9cda","color":"#fff", "border":"1px solid #4c9cda"})
