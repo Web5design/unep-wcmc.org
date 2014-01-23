4f2e33ec12340b26a9f5e4e8715875da979a3fae
Blanca Tortajada <tortajadablanca@gmail.com>
2014-01-23 12:06:09 +0000

    feedback changes

--- js/interactions.js
+++ js/interactions.js
@@ -12,4 +12,21 @@ $(document).ready(function(){
     $('ul#fields-small').toggle('slow');
     $('button#fields-button').css({"background":"#4c9cda","color":"#fff", "border":"1px solid #4c9cda"})
   });
+  
+  $('div.button.submit#submit-application').click(function() {
+    $('div.container.status#ok').slideDown('normal');
+  });
+  
+  $('div.container.status#ok i.icon-chevron-up').click(function() {
+    $('div.container.status#ok').slideUp('normal');
+  });
+  
+  $('ul.subnav-small li.header#strengthen-button i').click(function() {
+    $('ul.expertise-small#strengthen').toggle('normal');
+  });
+  
+  $('ul.subnav-small li.header#specialise-button i').click(function() {
+    $('ul.expertise-small#specialise').toggle('normal');
+  });
+  
 });
